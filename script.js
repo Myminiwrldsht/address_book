dc06dbc6c81a820230fed4ceecd5509cb2c65428
Myminiwrldsht <lakeishamaya46@gmail.com>
2026-05-15 10:13:05 +0300

    feat: add address book contact constructor.

---
+++ script.js
@@ -0,0 +1,9 @@
+// CONSTRUCTOR
+function Contact(firstName, lastName, address, phone, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.phone = phone;
+    this.email = email;
+}
+
